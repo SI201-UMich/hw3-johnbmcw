c3dfba78e4214317ad4ff7a276f934700ccd2960
John McWilliams <johnmcwilliams@Johns-MacBook-Air-45.local>
2026-01-30 16:22:31 -0500

    message

--- HW3.py
+++ HW3.py
@@ -1,6 +1,6 @@
-# Name:
-# Student ID:
-# Email:
+# Name: John McWilliams
+# Student ID: 3485 4614
+# Email: johnmcw@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
 # e.g.:
@@ -33,7 +33,9 @@ class CouponDispenser:
             coupon_cards (list[str]): list of possible coupons users can receive.
         """
         # TODO: Implement per instructions
-        pass
+        self.coupon_cards = coupon_cards
+        self.customer_roster = []
+        self.issued_indices = []
 
     def __str__(self):
         """
